b1f9f7f4d975b3bb9a53ef7cc2e058dce2b013fc
“ilhanu” <ilhanunlu@gmail.com>
2022-04-05 14:18:01 +0200

    [WIP] removed unnecessary code

--- pallets/fund/src/lib.rs
+++ pallets/fund/src/lib.rs
@@ -49,18 +49,6 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
 	// used to generate sovereign account
     
-	#[pallet::constant]
-    type PalletId: Get<PalletId>;
-
-	/// The currency in which the housefund will be denominated
-	type Currency: ReservableCurrency<Self::AccountId>;}
-
-    pub type HousingFund = u32;
-
-    type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
-    type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
-    type FundOf<T> = HousingFund<AccountIdOf<T>, <T as frame_system::Config>::BlockNumber>;
-    type ContributionOf<T> = Contribution<AccountIdOf<T>, BalanceOf<T>, <T as frame_system::Config>::BlockNumber>;
 
 
     // Grant in round
@@ -92,9 +80,7 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T> where Balance = BalanceOf<T>, AccountId = <T as frame_system::Config>::AccountId, <T as frame_system::Config>::BlockNumber {
 		ContributeSucceed(AccountId, Balance, BlockNumber),
-		Contribution
-		GrantApproved(RoundIndex, FundIndex),
-		RoundCanceled(RoundIndex),
+		Contribution(),	
 		FundSucceed(),
 	}
 
@@ -127,40 +113,6 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 
-		/// Create Fund
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(2,2)]
-		pub fn create_Fund(origin, name: Vec<u8>, logo: Vec<u8>, description: Vec<u8>, website: Vec<u8>) {
-			let who = ensure_signed(origin)?;
-
-			
-			let index = FundCount::get();
-			let next_index = index.checked_add(1).ok_or(Error::<T>::Overflow)?;
-
-			// Add grant to list
-			<Funds<T>>::insert(index, Fund);
-			FundCount::put(next_index);
-
-			Self::deposit_event(RawEvent::FundCreated(index));
-		}
-
-		/// Funding to matching fund pool
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn fund(origin, fund_balance: BalanceOf<T>) {
-			let who = ensure_signed(origin)?;
-			ensure!(fund_balance > (0 as u32).into(), Error::<T>::InvalidParam);
-
-			// No fees are paid here if we need to create this account; that's why we don't just
-			// use the stock `transfer`.
-			<T as Config>::Currency::resolve_creating(&Self::account_id(), <T as Config>::Currency::withdraw(
-				&who,
-				fund_balance,
-				WithdrawReasons::from(WithdrawReasons::TRANSFER),
-				ExistenceRequirement::AllowDeath,
-			)?);
-
-			Self::deposit_event(RawEvent::FundSucceed());
-		}
-
 
 		/// Contribute a grant
 		#[weight = 10_000 + T::DbWeight::get().reads_writes(2,1)]
@@ -171,42 +123,6 @@ pub mod pallet {
 			ensure!(Fund_index < Fund_count, Error::<T>::InvalidParam);
 			let now = <frame_system::Module<T>>::block_number();
 			
-			// round list must be not none
-			let round_index = RoundCount::get();
-			ensure!(round_index > 0, Error::<T>::NoActiveRound);
-
-			// Find processing round
-			let mut processing_round: Option<RoundOf::<T>> = None;
-			for i in (0..round_index).rev() {
-				let round = <Rounds<T>>::get(i).unwrap();
-				if !round.is_canceled && round.start < now && round.end > now {
-					processing_round = Some(round);
-				}
-			}
-
-			let mut round = processing_round.ok_or(Error::<T>::RoundNotProcessing)?;
-
-			// Find grant by index
-			let mut found_grant: Option<&mut GrantOf::<T>> = None;
-			for grant in round.grants.iter_mut() {
-				if grant.Fund_index == Fund_index {
-					found_grant = Some(grant);
-					break;
-				}
-			}
-
-			let grant = found_grant.ok_or(Error::<T>::NoActiveGrant)?;
-			ensure!(!grant.is_canceled, Error::<T>::GrantCanceled);
-
-			// Find previous contribution by account_id
-			// If you have contributed before, then add to that contribution. Otherwise join the list.
-			let mut found_contribution: Option<&mut ContributionOf::<T>> = None;
-			for contribution in grant.contributions.iter_mut() {
-				if contribution.account_id == who {
-					found_contribution = Some(contribution);
-					break;
-				}
-			}
 
 			match found_contribution {
 				Some(contribution) => {
@@ -228,8 +144,6 @@ pub mod pallet {
 				ExistenceRequirement::AllowDeath
 			)?;
 			
-			<Rounds<T>>::insert(round_index-1, round);
-
 			Self::deposit_event(RawEvent::ContributeSucceed(who, Fund_index, value, now));
 		}
 
@@ -240,43 +154,6 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 			let now = <frame_system::Module<T>>::block_number();
 
-			// Only Fund owner can withdraw
-			let Fund = Funds::<T>::get(Fund_index).ok_or(Error::<T>::NoActiveGrant)?;
-			ensure!(who == Fund.owner, Error::<T>::InvalidAccount);
-
-			let mut round = <Rounds<T>>::get(round_index).ok_or(Error::<T>::NoActiveRound)?;
-			let mut found_grant: Option<&mut GrantOf::<T>> = None;
-			for grant in round.grants.iter_mut() {
-				if grant.Fund_index == Fund_index {
-					found_grant = Some(grant);
-					break;
-				}
-			}
-
-			let grant = found_grant.ok_or(Error::<T>::NoActiveGrant)?;
-			ensure!(now <= grant.withdrawal_expiration, Error::<T>::WithdrawalExpirationExceed);
-
-			// This grant must not have distributed funds
-			ensure!(grant.is_approved, Error::<T>::GrantNotApproved);
-			ensure!(!grant.is_withdrawn, Error::<T>::GrantWithdrawn);
-
-			// Calculate contribution amount
-			let mut contribution_amount: BalanceOf<T>  = (0 as u32).into();
-			for contribution in grant.contributions.iter() {
-				let contribution_value = contribution.value;
-				contribution_amount += contribution_value;
-			}
-
-			let matching_fund = grant.matching_fund;
-
-			// Return funds to caller without charging a transfer fee
-			let _ = <T as Config>::Currency::resolve_into_existing(&Fund.owner, <T as Config>::Currency::withdraw(
-				&Self::account_id(),
-				matching_fund,
-				WithdrawReasons::from(WithdrawReasons::TRANSFER),
-				ExistenceRequirement::AllowDeath,
-			)?);
-
 			// Distribute contribution amount
 			let _ = <T as Config>::Currency::resolve_into_existing(&Fund.owner, <T as Config>::Currency::withdraw(
 				&Self::Fund_account_id(Fund_index),
@@ -285,7 +162,6 @@ pub mod pallet {
 				ExistenceRequirement::AllowDeath,
 			)?);
 
-
 			// Set is_withdrawn
 			grant.is_withdrawn = true;
 			grant.withdrawal_expiration = now + <WithdrawalExpiration<T>>::get();
@@ -293,23 +169,6 @@ pub mod pallet {
 			<Rounds<T>>::insert(round_index, round.clone());
 
 			Self::deposit_event(RawEvent::GrantWithdrawn(round_index, Fund_index, matching_fund, contribution_amount));
-		}
-
-
-		/// Set max grant count per round
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn set_max_grant_count_per_round(origin, max_grant_count_per_round: u32) {
-			ensure_root(origin)?;
-			ensure!(max_grant_count_per_round > 0, Error::<T>::InvalidParam);
-			MaxGrantCountPerRound::put(max_grant_count_per_round);
-		}
-
-		/// Set withdrawal expiration
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn set_withdrawal_expiration(origin, withdrawal_expiration: T::BlockNumber) {
-			ensure_root(origin)?;
-			ensure!(withdrawal_expiration > (0 as u32).into(), Error::<T>::InvalidParam);
-			<WithdrawalExpiration<T>>::put(withdrawal_expiration);
 		}
 
 
@@ -322,53 +181,12 @@ impl<T: Config> Module<T> {
 		return T::PalletId::get().into_account();
 	}
 
-	pub fn Fund_account_id(index: ) -> T::AccountId {
-		T::ModuleId::get().into_sub_account(index)
-	}
-
-	/// Get all Funds
-	pub fn get_Funds() -> Vec<Fund<AccountIdOf<T>, T::BlockNumber>> {
-		let len = FundCount::get();
-		let mut Funds: Vec<Fund<AccountIdOf<T>, T::BlockNumber>> = Vec::new();
-		for i in 0..len {
-			let Fund = <Funds<T>>::get(i).unwrap();
-			Funds.push(Fund);
-		}
-		Funds
-	}
-
+	
 	// Calculate used funds
 	pub fn get_used_fund() -> BalanceOf<T> {
 		let now = <frame_system::Module<T>>::block_number();
 		let mut used_fund: BalanceOf<T> = (0 as u32).into();
-		let count = RoundCount::get();
 
-		for i in 0..count {
-			let round = <Rounds<T>>::get(i).unwrap();
-
-			// The cancelled round does not occupy funds
-			if round.is_canceled {
-				continue;
-			}
-
-			let grants = &round.grants;
-
-			// Rounds that are not finalized always occupy funds
-			if !round.is_finalized {
-				used_fund += round.matching_fund;
-				continue;
-			}
-
-			for grant in grants.iter() {
-				// If the undrawn funds expire, they will be returned to the foundation.
-				if grant.is_approved && !grant.is_withdrawn && grant.withdrawal_expiration > now {
-					continue;
-				}
-
-				// Because the funds that have been withdrawn are no longer in the foundation account, they will not be recorded.
-				if grant.is_withdrawn {
-					continue;
-				}
 
 				used_fund += grant.matching_fund;
 			}
@@ -383,20 +201,17 @@ impl<AccountId, Balance: From<u32>, BlockNumber: From<u32>> Round<AccountId, Bal
 		let mut grant_round  = Round {
 			start: start,
 			end: end,
-			matching_fund: matching_fund,
-			grants: Vec::new(),
+			owner: BoundedVec<owner>,
+			amount: amount,
 			is_canceled: false,
 			is_finalized: false,
+			nft:nft
 		};
+	fn select_contributor(){};
 
-		for HousingFund in HousingFund {
-			grant_round.grants.push(Grant {
-				Fund_index: Fund_index,
-				contributions: Vec::new(),
-				withdrawal_expiration: (0 as u32).into(),
-				matching_fund: (0 as u32).into(),
-			});
-		}
+		//TODO:logic of fair contribution
+		
+		
 	}
 	grant_round
 }
